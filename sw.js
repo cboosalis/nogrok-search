b03a26194eb643e7a5ca1c5181f1c2a51ed5b560
Chris Boosalis <chris.boosalis@csus.edu>
2026-06-28 03:44:21 -0700

    Remove caching and fetch event from service worker
    Removed cache handling and fetch event listener.

--- sw.js
+++ sw.js
@@ -1,39 +1,10 @@
-const CACHE_NAME = "no-grok-search-v8";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
-
   self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
 });
