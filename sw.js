10083685523fe90eeb8589e4a83d57d7aadb0906
Chris Boosalis <chris.boosalis@csus.edu>
2026-06-28 03:17:06 -0700

    Update service worker caching strategy

--- sw.js
+++ sw.js
@@ -1,20 +1,39 @@
+const CACHE_NAME = "no-grok-search-v4";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("no-grok-search-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./app.js",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
     })
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
